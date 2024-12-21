f92ad8cf70ad97383748965ce2f796bc10883f32
Xeron <cw48565@gmail.com>
2024-12-21 23:49:32 +0800

    2024.12.21

--- 2.两数相加.py
+++ 2.两数相加.py
@@ -12,7 +12,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
+        dummy = ListNode()
+        current = dummy
+        carry = 0
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            sum = x + y + carry
+            carry = sum // 10
+            current.next = ListNode(sum % 10)
+            current = current.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        if carry:
+            current.next = ListNode(carry)
+        return dummy.next
         
 # @lc code=end
 
---
+++ 3.无重复字符的最长子串.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=3 lang=python3
+#
+# [3] 无重复字符的最长子串
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_index = {}
+        left = 0
+        max_length = 0
+        for right, char in enumerate(s):
+            if char in char_index and char_index[char] >= left:
+                left = char_index[char] + 1
+            char_index[char] = right
+            max_length = max(max_length, right - left + 1)
+        return max_length
+# @lc code=end
+
