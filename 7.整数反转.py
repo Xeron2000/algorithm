b581e169536f8cee7dbd08644dd00c01dd55fe07
Xeron <cw48565@gmail.com>
2025-01-26 23:23:49 +0800

    2025.1.26

--- 7.整数反转.py
+++ 7.整数反转.py
@@ -7,18 +7,16 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
-        stack = []
-        if x < 0:
-            stack.append('-')
-            x = abs(x)
-        while x > 0:
-            stack.append(x % 10)
-            x = x // 10
-        res = 0
-        while stack:
-            res = res * 10 + stack.pop()
-        if res > 2**31 - 1 or res < -2**31:
+
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        
+        reversed_num = int(str(x)[::-1]) * sign
+        
+        if reversed_num < -2**31 or reversed_num > 2**31 - 1:
             return 0
-        return res
+        
+        return reversed_num
+
 # @lc code=end
 
